e94f9a1b7c2158be1bfef0de25f2cd27ea30dfe1
hendrickl <h.lincertin@gmail.com>
2024-07-30 15:34:17 +0200

    Unity scripting task 2

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player; 
+    private Vector3 _offset;   
+
+	// Use this for initialization
+	void Start () 
+	{
+		_offset = transform.position - player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate() 
+	{
+		transform.position = player.transform.position + _offset;
+	} 
+}
