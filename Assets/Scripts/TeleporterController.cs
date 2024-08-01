5206e772bfa7300ebcef46d507fc4aadf825f8b9
hendrickl <h.lincertin@gmail.com>
2024-08-01 11:16:19 +0200

    Setup for next project: unity-ui

---
+++ Assets/Scripts/TeleporterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterController : MonoBehaviour 
+{
+	public GameObject linkedTeleporter;
+	private bool _isTeleported;
+	private Transform _currentTeleporterTransform;
+
+	private void Start() 
+	{
+		_isTeleported = false;	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+		while (_isTeleported == true)
+		{
+			if (other.CompareTag("Player"))
+        	{
+        	    other.transform.position = linkedTeleporter.transform.position;
+				_isTeleported = false;
+        	}		
+		}
+    }
+
+	private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _isTeleported = false;
+        }
+    }
+}
